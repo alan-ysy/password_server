fb95096dea035c37f0a795c8eec3609b7d50d80c
Alan Yeung <184780131+alan-ys-yeung@users.noreply.github.com>
2026-04-15 11:50:06 -0400

    Initial commit

---
+++ src/test/java/com/example/password_server/PasswordServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.password_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
