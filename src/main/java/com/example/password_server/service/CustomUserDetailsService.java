bc8c38d9afe5bdf4c3436bc734b6de8e40b2261f
Alan Yeung <184780131+alan-ys-yeung@users.noreply.github.com>
2026-04-16 17:29:37 -0400

    Modify service

--- src/main/java/com/example/password_server/service/CustomUserDetailsService.java
+++ src/main/java/com/example/password_server/service/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package com.example.password_server.service;
 
 import com.example.password_server.model.User;
 import com.example.password_server.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,8 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
